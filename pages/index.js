677a54f118c403ba10017561e365918136194daf
Raj Kanani <rajkanani1206@gmail.com>
2024-08-07 22:35:31 +0200

    Initial Commit

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Layout from "@/components/Layout";
+
+export default function Home() {
+  return (
+    <Layout>
+      <div className="text-blue-900 flex justify-between">
+        <h2>Hello Raj,</h2>
+      </div>
+    </Layout>
+  );
+}
